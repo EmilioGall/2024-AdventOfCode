9459cf63814ae7551312a74f7c9b45c96dcd997b
Emilio Gallo <emilio.gallo17@gmail.com>
2024-12-02 15:26:14 +0100

    feat: added event listener to button - Dec_01/js/main.js

--- Dec_01/js/main.js
+++ Dec_01/js/main.js
@@ -1,2 +1,14 @@
 console.log('///// Main /////');
 
+// Create a debounced version of the [executeBoolkaichiTournament] function with 500ms delay
+const debouncedCalculation = debounce(() => calculateTotalDistance(leftList, rightList), 500);
+
+// Add keypress event listener on [inputPasswordElem]
+calculateButtonElem.addEventListener('click', function (e) {
+
+   // console.log('Button clicked');
+
+   // Call debounced version of controls
+   debouncedCalculation();
+
+});
