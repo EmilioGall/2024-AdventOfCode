934b57f940f3168847352cd682802589d120dfc6
Emilio Gallo <emilio.gallo17@gmail.com>
2024-12-02 15:48:15 +0100

    feat: added debounced version of calculateSimilarityScore( ) function to event listener - Dec_01/js/main.js

--- Dec_01/js/main.js
+++ Dec_01/js/main.js
@@ -1,14 +1,20 @@
 console.log('///// Main /////');
 
-// Create a debounced version of the [executeBoolkaichiTournament] function with 500ms delay
-const debouncedCalculation = debounce(() => calculateTotalDistance(leftList, rightList), 500);
+// Create a debounced version of the [calculateTotalDistance] function with 500ms delay
+const debounceDistance = debounce(() => calculateTotalDistance(leftList, rightList), 500);
+
+// Create a debounced version of the [calculateSimilarityScore] function with 500ms delay
+const debounceSimilarity = debounce(() => calculateSimilarityScore(leftList, rightList), 500);
 
 // Add keypress event listener on [inputPasswordElem]
 calculateButtonElem.addEventListener('click', function (e) {
 
    // console.log('Button clicked');
 
-   // Call debounced version of controls
-   debouncedCalculation();
+   // Call debounced version of function [calculateTotalDistance]
+   debounceDistance();
+
+   // Call debounced version of function [calculateSimilarityScore]
+   debounceSimilarity();
 
 });
