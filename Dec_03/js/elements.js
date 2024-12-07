34bbed8599e9375cd1ddfbaa990727e873f46629
Emilio Gallo <emilio.gallo17@gmail.com>
2024-12-07 18:43:31 +0100

    feat: project scaffolding - Dec_03

---
+++ Dec_03/js/elements.js
@@ -0,0 +1,10 @@
+console.log('///// Elements /////');
+
+
+
+
+const calculateButtonElem = document.querySelector(".btn");
+
+const outputDiv = document.getElementById('output');
+
+const outputDiv2 = document.getElementById('output2');
---
+++ Dec_03/js/main.js
@@ -0,0 +1,9 @@
+console.log('///// Main /////');
+
+
+// Add keypress event listener on [calculateButtonElem]
+calculateButtonElem.addEventListener('click', function (e) {
+
+   // console.log('Button clicked');
+
+});
