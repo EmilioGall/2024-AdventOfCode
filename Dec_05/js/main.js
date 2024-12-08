16fb3ddf532f58b736fa5531ae512e68710bb617
Emilio Gallo <emilio.gallo17@gmail.com>
2024-12-08 14:59:05 +0100

    feat: project scaffolding - Dec_05

---
+++ Dec_05/js/main.js
@@ -0,0 +1,10 @@
+console.log('///// Main /////');
+
+// Add keypress event listener on [calculateButtonElem]
+calculateButtonElem.addEventListener('click', function (e) {
+
+   console.log('Button clicked');
+
+
+
+});
